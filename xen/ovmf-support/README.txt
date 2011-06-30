fd266367ec36fdb225cdeb7818cb7b8686fe8f0b
gavinguan <gbtju85@gmail.com>
2011-07-01 01:01:22 +0800

    Modify the patches for publishing Xen SMBIOS inside UEFI

--- xen/ovmf-support/README.txt
+++ xen/ovmf-support/README.txt
@@ -1,26 +1,26 @@
+patch:
+-----
+ovmf_edk2_OvmfPkg_AcpiPlatformDxe.patch
+ovmf_edk2_OvmfPkg_XenSmbiosDxe.patch
+ovmf_edk2_OvmfPkg_configuration.patch
+ovmf_xen_smbios_acpi_publish.patch
+
+(PS: All the ovmf_edk2_* patches are included in one: ovmf_edk2_OvmfPkg_final.patch)
+
+binary file
+-----
+ovmf_binary_for_xen\ovmf-ia32.bin
+ovmf_binary_for_xen\ovmf-ia32-cirrus-vga.bin
+ovmf_binary_for_xen\ovmf-x64.bin
+ovmf_binary_for_xen\ovmf-x64-cirrus-vga.bin
+
 Xen ACPI Tables published inside UEFI
 -----
 1) The patches publish Xen ACPI tables inside UEFI. Support both QEMU and Xen.
 2) Create a new OvmfPkg/AcpiPlatformDxe to replace MdeModulePkg/Universal/Acpi/AcpiPlatformDxe.
 3) OvmfPkg/AcpiPlatformDxe detects the Xen environment, finds out Xen ACPI Tables and publishes them with the Acpi Table protocol. If it is not the Xen environment, OvmfPkg/AcpiPlatformDxe just does as MdeModulePkg/Universal/Acpi/AcpiPlatformDxe.
 
-patch:
-ovmf_edk2_OvmfPkg_AcpiPlatformDxe.patch
-ovmf_edk2_OvmfPkg_configuration.patch
-ovmf_xen_smbios_acpi_publish.patch
-ovmf_binary_for_xen\ovmf-ia32.bin
-ovmf_binary_for_xen\ovmf-ia32-cirrus-vga.bin
-ovmf_binary_for_xen\ovmf-x64.bin
-ovmf_binary_for_xen\ovmf-x64-cirrus-vga.bin
-
-
 Xen SMBIOS published inside UEFI
 -----
-1) The patches publish Xen SMBIOS inside UEFI. They support both QEMU and Xen.
-2) OvmfPkg/XenSmbiosDxe can detect the Xen environment and Xen hvmloader SMBIOS, and then installs Xen BIOS to UEFI system configuration table. 
-3) Then, MdeModulePkg/Universal/SmbiosDxe will look up the system table to see if a SMBIOS table has been install. If it has been installed, just does nothing.(Support Xen environment) Otherwise, it does as before.(Support QEMU environment)
-
-patch:
-ovmf_edk2_OvmfPkg_XenSmbiosDxe.patch
-ovmf_edk2_SmbiosDxe.patch
-ovmf_xen_smbios_acpi_publish.patch
+1) The patches publish Xen SMBIOS inside UEFI. Support both QEMU and Xen.
+2) OvmfPkg/XenSmbiosDxe can detect the Xen environment and Xen hvmloader SMBIOS. After that, it parses Xen BIOS structure and calls EFI_SMBIOS_PROTOCOL=>Add to add them to UEFI system.
--- xen/ovmf-support/XenSmbiosDxe/XenSmbiosDxe.h
+++ xen/ovmf-support/XenSmbiosDxe/XenSmbiosDxe.h
@@ -38,4 +38,123 @@
 
 extern EFI_GUID gEfiXenInfoGuid;
 
+/**
+  Figures out if Xen hvmloader provides SMBIOS for HVM.
+
+  @return SMBIOS_TABLE_ENTRY_POINT   Address of Xen hvmloader SMBIOS.
+
+**/
+SMBIOS_TABLE_ENTRY_POINT *
+XenSmbiosDetect (
+  VOID
+  );
+
+/**
+  Get SMBIOS record by type.
+
+  @param  Smbios       SMBIOS entry point structure pointer.
+  @param  Type         Type of SMBIOS record.
+  @param  Index        Index.
+
+**/
+SMBIOS_STRUCTURE_POINTER
+GetSmbiosTableFromType (
+  IN SMBIOS_TABLE_ENTRY_POINT  *Smbios,
+  IN UINT8                 Type,
+  IN UINTN                 Index
+  );
+
+/**
+  Get SMBIOS String.
+
+  @param  SmbiosTable   SMBIOS pointer.
+  @param  String        SMBIOS string.
+
+**/
+CHAR8 *
+GetSmbiosString (
+  IN SMBIOS_STRUCTURE_POINTER  SmbiosTable,
+  IN SMBIOS_TABLE_STRING       String
+  );
+
+/**
+  Logs SMBIOS record.
+
+  @param  Smbios   Pointer to SMBIOS protocol instance.
+  @param  Buffer   Pointer to the data buffer.
+
+**/
+VOID
+LogSmbiosData (
+  IN   EFI_SMBIOS_PROTOCOL        *Smbios,
+  IN   UINT8                      *Buffer
+  );
+
+/**
+  Get the SMBIOS recorde from Xen SMBIOS entry point and install it.
+
+  Type 0    BIOS Information
+  Type 1    System Information
+  Type 3    System Enclosure or Chassis
+  Type 4    Processor Information
+  Type 11   OEM Strings
+  Type 16   Physical Memory Array
+  Type 17   Memory Device
+  Type 19   Memory Array Mapped Address
+  Type 20   Memory Device Mapped Address
+  Type 32   System Boot Information
+
+  @return SMBIOS_TABLE_ENTRY_POINT   Address of Xen hvmloader SMBIOS.
+
+**/
+VOID
+InstallSmbiosType0 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType1 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType3 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType4 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType11 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType16 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType17 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType19 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType20 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
+VOID
+InstallSmbiosType32 (
+  IN SMBIOS_TABLE_ENTRY_POINT       *SmbiosEntry
+  );
+
 #endif
